027c8fb7531c5ab3e8b23af49064ee3eeb67696d
LukeKeltner <lkeltner87@gmail.com>
2018-03-05 20:54:38 -0500

    updated horizontal projectile questions

---
+++ db/seeds/2D Kinematics/horizontalprojectiles.sql
@@ -0,0 +1,153 @@
+insert into questions(topic, subtopic, question) values
+(
+    "2D Kinematics",
+    "Horizontal Projectiles",
+    '{
+        "text":"A ball is launched completely horizontally off a rand0 m cliff with an initial velocity of rand1 m/s. How far in meters from the base of the cliff does the ball get by the time it hits the water?  Use the acceleration due to gravity to be 10 m/s/s.",
+        "correct":["rand1*sqrt(rand0/5)"],
+        "wrong":["rand1*sqrt(rand0/5)*rand2", "rand1*sqrt(rand0/5)*rand3", "rand1*sqrt(rand0/5)*rand4"],
+        "img":"horizontalprojectile.jpg",
+        "random":5,
+        "rand0": [10,100,0],
+        "rand1": [1,10,2],
+        "rand2": [0.9,1.1,2],
+        "rand3": [0.9,1.1,2],
+        "rand4": [0.9,1.1,2]
+    }'
+),
+
+(
+    "2D Kinematics",
+    "Horizontal Projectiles",
+    '{
+        "text":"A ball is launched completely horizontally off a cliff with an initial velocity of rand0 m/s. The ball ends up rand1 m away from the base of the cliff.  How high is the cliff in meters?  Use the acceleration due to gravity to be 10 m/s/s.",
+        "correct":["5*(rand1/rand0)^2"],
+        "wrong":["5*(rand1/rand0)^2*rand2", "5*(rand1/rand0)^2*rand3", "5*(rand1/rand0)^2*rand4"],
+        "img":"horizontalprojectile.jpg",
+        "random":5,
+        "rand0": [1,10,2],
+        "rand1": [30,100,0],
+        "rand2": [0.9,1.1,2],
+        "rand3": [0.9,1.1,2],
+        "rand4": [0.9,1.1,2]
+    }'
+),
+
+(
+    "2D Kinematics",
+    "Horizontal Projectiles",
+    '{
+        "text":"A ball is launched completely horizontally off a rand0 m cliff with some initial velocity. The ball ends up rand1 m away from the base of the cliff.  What must have been the initial velocity of the ball in meters per second?  Use the acceleration due to gravity to be 10 m/s/s.",
+        "correct":["sqrt(5/rand0)*rand1"],
+        "wrong":["sqrt(5/rand0)*rand1*rand2", "sqrt(5/rand0)*rand1*rand3", "sqrt(5/rand0)*rand1*rand4"],
+        "img":"horizontalprojectile.jpg",
+        "random":5,
+        "rand0": [10,100,0],
+        "rand1": [10,50,2]
+        "rand2": [0.9,1.1,2],
+        "rand3": [0.9,1.1,2],
+        "rand4": [0.9,1.1,2]
+    }'
+),
+
+(
+    "2D Kinematics",
+    "Horizontal Projectiles",
+    '{
+        "text":"A ball is launched completely horizontally off a rand0 m cliff with an initial velocity of rand1 m/s. How far in meters from the base of the cliff does the ball get by the time it hits the water?  Use the acceleration due to gravity to be 10 m/s/s.",
+        "correct":["rand1*sqrt(rand0/5)"],
+        "wrong":["rand1*sqrt(rand0/5)*rand2", "rand1*sqrt(rand0/5)*rand3", "rand1*sqrt(rand0/5)*rand4"],
+        "img":"horizontalprojectile.jpg",
+        "random":5,
+        "rand0": [10,100,0],
+        "rand1": [1,10,2],
+        "rand2": [0.9,1.1,2],
+        "rand3": [0.9,1.1,2],
+        "rand4": [0.9,1.1,2]
+    }'
+),
+
+(
+    "2D Kinematics",
+    "Horizontal Projectiles",
+    '{
+        "text":"A ball is launched completely horizontally off a cliff with an initial velocity of rand0 m/s. The ball ends up rand1 m away from the base of the cliff.  How high is the cliff in meters?  Use the acceleration due to gravity to be 10 m/s/s.",
+        "correct":["5*(rand1/rand0)^2"],
+        "wrong":["5*(rand1/rand0)^2*rand2", "5*(rand1/rand0)^2*rand3", "5*(rand1/rand0)^2*rand4"],
+        "img":"horizontalprojectile.jpg",
+        "random":5,
+        "rand0": [1,10,2],
+        "rand1": [30,100,0],
+        "rand2": [0.9,1.1,2],
+        "rand3": [0.9,1.1,2],
+        "rand4": [0.9,1.1,2]
+    }'
+),
+
+(
+    "2D Kinematics",
+    "Horizontal Projectiles",
+    '{
+        "text":"A ball is launched completely horizontally off a rand0 m cliff with some initial velocity. The ball ends up rand1 m away from the base of the cliff.  What must have been the initial velocity of the ball in meters per second?  Use the acceleration due to gravity to be 10 m/s/s.",
+        "correct":["sqrt(5/rand0)*rand1"],
+        "wrong":["sqrt(5/rand0)*rand1*rand2", "sqrt(5/rand0)*rand1*rand3", "sqrt(5/rand0)*rand1*rand4"],
+        "img":"horizontalprojectile.jpg",
+        "random":5,
+        "rand0": [10,100,0],
+        "rand1": [10,50,2]
+        "rand2": [0.9,1.1,2],
+        "rand3": [0.9,1.1,2],
+        "rand4": [0.9,1.1,2]
+    }'
+),
+
+(
+    "2D Kinematics",
+    "Horizontal Projectiles",
+    '{
+        "text":"A ball is launched completely horizontally off a rand0 m cliff with an initial velocity of rand1 m/s. How far in meters from the base of the cliff does the ball get by the time it hits the water?  Use the acceleration due to gravity to be 10 m/s/s.",
+        "correct":["rand1*sqrt(rand0/5)"],
+        "wrong":["rand1*sqrt(rand0/5)*rand2", "rand1*sqrt(rand0/5)*rand3", "rand1*sqrt(rand0/5)*rand4"],
+        "img":"horizontalprojectile.jpg",
+        "random":5,
+        "rand0": [10,100,0],
+        "rand1": [1,10,2],
+        "rand2": [0.9,1.1,2],
+        "rand3": [0.9,1.1,2],
+        "rand4": [0.9,1.1,2]
+    }'
+),
+
+(
+    "2D Kinematics",
+    "Horizontal Projectiles",
+    '{
+        "text":"A ball is launched completely horizontally off a cliff with an initial velocity of rand0 m/s. The ball ends up rand1 m away from the base of the cliff.  How high is the cliff in meters?  Use the acceleration due to gravity to be 10 m/s/s.",
+        "correct":["5*(rand1/rand0)^2"],
+        "wrong":["5*(rand1/rand0)^2*rand2", "5*(rand1/rand0)^2*rand3", "5*(rand1/rand0)^2*rand4"],
+        "img":"horizontalprojectile.jpg",
+        "random":5,
+        "rand0": [1,10,2],
+        "rand1": [30,100,0],
+        "rand2": [0.9,1.1,2],
+        "rand3": [0.9,1.1,2],
+        "rand4": [0.9,1.1,2]
+    }'
+),
+
+(
+    "2D Kinematics",
+    "Horizontal Projectiles",
+    '{
+        "text":"A ball is launched completely horizontally off a rand0 m cliff with some initial velocity. The ball ends up rand1 m away from the base of the cliff.  What must have been the initial velocity of the ball in meters per second?  Use the acceleration due to gravity to be 10 m/s/s.",
+        "correct":["sqrt(5/rand0)*rand1"],
+        "wrong":["sqrt(5/rand0)*rand1*rand2", "sqrt(5/rand0)*rand1*rand3", "sqrt(5/rand0)*rand1*rand4"],
+        "img":"horizontalprojectile.jpg",
+        "random":5,
+        "rand0": [10,100,0],
+        "rand1": [10,50,2]
+        "rand2": [0.9,1.1,2],
+        "rand3": [0.9,1.1,2],
+        "rand4": [0.9,1.1,2]
+    }'
+);
